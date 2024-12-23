8c32828436197ca627390a4a97e684d9abec6e95
ReadMyName <qazwsx5219430630@gmail.com>
2024-12-23 09:52:18 +0800

    Initial Commit

---
+++ automation-testing/automation_testing.h
@@ -0,0 +1,18 @@
+//
+//  automation_testing.h
+//  automation-testing
+//
+//  Created by 鍾哲玄 on 2024/12/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for automation_testing.
+FOUNDATION_EXPORT double automation_testingVersionNumber;
+
+//! Project version string for automation_testing.
+FOUNDATION_EXPORT const unsigned char automation_testingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <automation_testing/PublicHeader.h>
+
+
